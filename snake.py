c945d7f4c8827e2e11efb30f0c31cfac8a036a23
lostfile <lostfile14@aol.com>
2019-10-18 17:45:49 -0400

    Update snake.py
    added ping

--- snake.py
+++ snake.py
@@ -6,6 +6,17 @@ print("snakebot v1.0")
 ircsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server = input("Host:")
 port = input("Port number:")
-nick = input("botnick:")
-channel = input("channle:")
+botnick = input("botnick:")
+channel = input("channel:")
+exitcode = "bye " + botnick
+ircsock.connect((server, port)) 
+ircsock.send(bytes("USER "+ botnick +" "+ botnick +" "+ botnick + " " + botnick + "n", "UTF-8")) 
+ircsock.send(bytes("NICK "+ botnick +"n", "UTF-8"))
 
+while 1:    
+   text=irc.recv(2040)  
+   print text
+ if text.find('PING') != -1:
+    irc.send('PONG ' + text.split() [1] + '\r\n')
+    
+ 
