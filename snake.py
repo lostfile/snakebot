825b683e9b7dabb876980d3dfbdc17e9516e4c3e
lostfile <lostfile14@aol.com>
2019-10-18 17:31:08 -0400

    Update snake.py
    added user inputs

--- snake.py
+++ snake.py
@@ -2,10 +2,10 @@
 import socket
 import sys
 import time
-
+print("snakebot v1.0")
 ircsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server = 'chat.freenode.com'
-port = 6697
-nick = 'snakebot'
-channel = '#snake'
+server = input("Host:")
+port = input("Port number:")
+nick = input("botnick:")
+channel = input("channle:")
 
