2800366927185e8ec7f39e2c6c0812f62233b76b
lostfile <lostfile14@aol.com>
2019-10-18 18:01:12 -0400

    Update snake.py

--- snake.py
+++ snake.py
@@ -13,13 +13,14 @@ ircsock.connect((server, port))
 ircsock.send(bytes("USER "+ botnick +" "+ botnick +" "+ botnick + " " + botnick + "n", "UTF-8")) 
 ircsock.send(bytes("NICK "+ botnick +"n", "UTF-8"))
 ircsock.send("JOIN "+ channel +"\n")
+
 while 1:    
    text=irc.recv(2040)  
    print text
  if text.find('PING') != -1:
     ircsock.send('PONG ' + text.split() [1] + '\r\n')
  if text.find('!time') != -1:
-    ircsock.send(
+    ircsock.send('the time is: ' + text.split() [1] + '\r\n')
 
     
  
